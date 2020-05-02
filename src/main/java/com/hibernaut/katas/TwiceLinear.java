1d68a5deb65a26cfcc57f6a1b4f45930f7c243f4
Grigoriy Petrenko <gregory.petrenko@gmail.com>
2020-05-02 03:46:10 +0300

    Add Twice Linear solution and tests

---
+++ src/main/java/com/hibernaut/katas/TwiceLinear.java
@@ -0,0 +1,64 @@
+package com.hibernaut.katas;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Consider a sequence u where u is defined as follows:
+ * <p>
+ * The number u(0) = 1 is the first one in u.
+ * For each x in u, then y = 2 * x + 1 and z = 3 * x + 1 must be in u too.
+ * There are no other numbers in u.
+ * <p>
+ * Ex: u = [1, 3, 4, 7, 9, 10, 13, 15, 19, 21, 22, 27, ...]
+ * <p>
+ * 1 gives 3 and 4, then 3 gives 7 and 10, 4 gives 9 and 13, then 7 gives 15 and 22 and so on...
+ * Task:
+ * <p>
+ * Given parameter n the function dbl_linear (or dblLinear...) returns the element u(n)
+ * of the ordered (with <) sequence u (so, there are no duplicates).
+ * Example:
+ * <p>
+ * dbl_linear(10) should return 22
+ * Note:
+ * <p>
+ * Focus attention on efficiency
+ */
+
+public class TwiceLinear {
+    public static int dblLinear (int n) {
+        ArrayList<Integer> sequence = new ArrayList();
+        int elementId = 0;
+        int initialElement;
+        int secondElement;
+        int thirdElement;
+
+        //Initializing starting element
+        sequence.add(elementId, 1);
+
+        while (elementId < n) {
+            //Putting current element into variable container
+            initialElement = sequence.get(elementId);
+
+            //Calculating second element and if it does not repeat, adding to sequence
+            secondElement = 2 * initialElement + 1;
+            if (!sequence.contains(secondElement)) {
+                sequence.add(secondElement);
+            }
+
+            //Calculating third element and if it does not repeat, adding to sequence
+            thirdElement = 3 * initialElement + 1;
+            if (!sequence.contains(thirdElement)) {
+                sequence.add(thirdElement);
+            }
+
+            //Increasing element ID
+            elementId++;
+
+            //Sorting the sequence
+            Collections.sort(sequence);
+        }
+        
+        return sequence.get(elementId);
+    }
+}
---
+++ src/test/java/com/hibernaut/katas/TwiceLinearTest.java
@@ -0,0 +1,20 @@
+package com.hibernaut.katas;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TwiceLinearTest {
+    private static void testing(int actual, int expected) {
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test() {
+        System.out.println("Fixed Tests dblLinear");
+        testing(TwiceLinear.dblLinear(10), 22);
+        testing(TwiceLinear.dblLinear(20), 57);
+        testing(TwiceLinear.dblLinear(30), 91);
+        testing(TwiceLinear.dblLinear(50), 175);
+    }
+}
