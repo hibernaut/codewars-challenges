43a92660216efcfacf3c6903b7fd026d5a7a4330
Grigoriy Petrenko <gregory.petrenko@gmail.com>
2020-06-18 01:55:50 +0300

    Adding some changes to EnoughIsEnough tests

---
+++ src/main/java/com/hibernaut/katas/Replacement.java
@@ -0,0 +1,7 @@
+package com.hibernaut.katas;
+
+/**
+ * Created by hibernaut on 18.06.2020.
+ */
+public class Replacement {
+}
---
+++ src/test/java/com/hibernaut/katas/ReplacementTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by hibernaut on 18.06.2020.
+ */
+public class ReplacementTest {
+
+}
