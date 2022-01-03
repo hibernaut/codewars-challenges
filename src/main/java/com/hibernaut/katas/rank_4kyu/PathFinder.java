0407b3c24fbea9f0f1a3578932e6677a05c6ddbf
Grigoriy <gregory.petrenko@gmail.com>
2022-01-04 01:19:02 +0200

    Add small code formatting to the PathFinder solution

--- src/main/java/com/hibernaut/katas/rank_4kyu/PathFinder.java
+++ src/main/java/com/hibernaut/katas/rank_4kyu/PathFinder.java
@@ -2,11 +2,11 @@ package com.hibernaut.katas.rank_4kyu;
 
 /**
  * Task
- *
+ * <p>
  * You are at position [0, 0] in maze NxN and you can only move in one of the four cardinal directions
  * (i.e. North, East, South, West). Return the minimal number of steps to exit position [N-1, N-1]
  * if it is possible to reach the exit from the starting position. Otherwise, return -1.
- *
+ * <p>
  * Empty positions are marked .. Walls are marked W. Start and exit positions are guaranteed to be empty
  * in all test cases.
  */
@@ -19,11 +19,11 @@ public class PathFinder {
         fillGraph(maze);
         moveTo(0, 0, 0);
 
-        if(graph[graph.length -1][graph.length - 1] == INF) {
+        if (graph[graph.length - 1][graph.length - 1] == INF) {
             return -1;
         }
 
-        return graph[graph.length -1][graph.length - 1];
+        return graph[graph.length - 1][graph.length - 1];
     }
 
     private static void fillGraph(String maze) {
@@ -32,7 +32,7 @@ public class PathFinder {
 
         for (int y = 0; y < linesArray.length; y++) {
             for (int x = 0; x < linesArray.length; x++) {
-                if(linesArray[y].charAt(x) == 'W') {
+                if (linesArray[y].charAt(x) == 'W') {
                     graph[y][x] = -1;
                 } else {
                     graph[y][x] = INF;
@@ -42,7 +42,7 @@ public class PathFinder {
     }
 
     private static void moveTo(int y, int x, int step) {
-        if(x == -1 || y == -1 || x == graph.length || y == graph.length || graph[y][x] <= step) {
+        if (x == -1 || y == -1 || x == graph.length || y == graph.length || graph[y][x] <= step) {
             return;
         }
 
