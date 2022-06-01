09cf5dbc48d87848b2eb65ddbd0ea03372d18363
Grigoriy <gregory.petrenko@gmail.com>
2022-06-01 19:13:33 +0300

    Add PersistenceBugger solution and tests

---
+++ src/test/java/com/hibernaut/katas/rank_6kyu/PersistentBuggerTest.java
@@ -0,0 +1,17 @@
+package com.hibernaut.katas.rank_6kyu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PersistentBuggerTest {
+
+    @Test
+    public void BasicTests() {
+        System.out.println("****** Basic Tests ******");
+        assertEquals(3, PersistentBugger.persistence(39));
+        assertEquals(0, PersistentBugger.persistence(4));
+        assertEquals(2, PersistentBugger.persistence(25));
+        assertEquals(4, PersistentBugger.persistence(999));
+    }
+}
